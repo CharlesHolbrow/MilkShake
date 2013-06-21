ec2ee635378939ac130584fa01f89168f9e1ce49
Charles Holbrow <charlesholbrow@gmail.com>
2013-06-20 23:09:36 -0700

    Javascripts for generating sessionDescriptions

---
+++ public/js/chrome.js
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////
+// Create Peer
+var config = {"iceServers":[{"url":"stun:stun.l.google.com:19302"}]};
+var constraints = {"optional": [{"DtlsSrtpKeyAgreement": true}, {"RtpDataChannels": true }] };
+var peerConnection = new webkitRTCPeerConnection(config, constraints);
+
+// triggered when an ice candidate is generated locally.
+// for  example by .setLocalDescription
+peerConnection.onicecandidate = function (event) {
+  // we have an ice candidate. 
+  // if event has a candidate, event.candidate looks something like this:
+  // {
+  //   sdpMLineIndex: 0, 
+  //   sdpMid: "data", 
+  //   candidate: "a=candidate:3408114210 1 udp 2113937151 10.0.1.46 52232 typ host generation 0↵"
+  // }
+  // We can now add it to the answerer. 
+  //if (!event || !event.candidate) return; answerer && answerer.addIceCandidate(event.candidate);
+  console.log("Got ICE Candidate:",  event.candidate);
+};
+
+
+///////////////////////////////////////////////////////////////
+// Create the dataChannel
+var dataChannel = peerConnection.createDataChannel('RTCDataChannel', {
+  //outOfOrderAllowed:true, maxRetransmitNum:0,
+  reliable: false // Chrome doesn't work without this. 
+});
+
+dataChannel.onmessage = function (event) { console.debug('received a message:', event.data); };
+dataChannel.onopen  = function (event) { dataChannel.send('Send text over RTP data ports'); };
+dataChannel.onclose = function (event) { console.error(event); };
+dataChannel.onerror = function (event) { console.error(event); };
+
+var mediaConstraints = {
+  optional: [],
+  mandatory: { OfferToReceiveAudio: false, OfferToReceiveVideo: false }
+};
+
+// offer must be created **after** we create the dataChannel
+peerConnection.createOffer(function (offerDescription) {
+  // sessionDescription will look something like this:
+  // {sdp: <text blob in SDP format>, type: "offer" }
+
+  // setLocalDescription will trigger multiple onicecandidate events
+  peerConnection.setLocalDescription(offerDescription);
+  // now peerConnection has a session description peerConnection.localDescription.sdp
+
+  // Now that we have a sessionDescription
+  console.log('Sess Desc:', offerDescription);
+  window.offerDescription = JSON.stringify(offerDescription);
+}, function(arg) {
+  console.log("Error creating offer:", arg);
+}, mediaConstraints);
---
+++ public/js/chromeAnswer.js
@@ -0,0 +1,44 @@
+var config = {"iceServers":[{"url":"stun:stun.l.google.com:19302"}]};
+var constraints = {"optional": [{"DtlsSrtpKeyAgreement": true}, {"RtpDataChannels": true }] };
+
+// Create Peer
+var peerConnection = new webkitRTCPeerConnection(config, constraints);
+
+// onicecandidate triggered when an ice candidate is generated
+// locally. for  example by .setLocalDescription
+peerConnection.onicecandidate = function (event) {
+  // we have an ice candidate - and can add it to the answerer. 
+  if (!event.candidate) return; 
+  peerConnection.addIceCandidate(event.candidate);
+  console.log("Got ICE Candidate:",  event.candidate);
+};
+
+
+// Create the dataChannel
+var dataChannel = peerConnection.createDataChannel('RTCDataChannel', {
+  reliable: false // Chrome doesn't work without this. 
+});
+
+var mediaConstraints = {
+  optional: [],
+  mandatory: { OfferToReceiveAudio: false, OfferToReceiveVideo: false }
+};
+
+dataChannel.onmessage = function (event) { console.debug('received a message:', event.data); };
+dataChannel.onopen  = function (event) { dataChannel.send('Send text over RTP data ports'); };
+dataChannel.onclose = function (event) { console.error(event); };
+dataChannel.onerror = function (event) { console.error(event); };
+
+
+// Create answer
+var createAnswer = function(offerDescription) {
+  console.log("before:", offerDescription);
+  offerDescription = new RTCSessionDescription(offerDescription);
+  console.log('okay', typeof offerDescription);
+  console.log("after:", offerDescription);
+  peerConnection.setRemoteDescription(offerDescription);
+  peerConnection.createAnswer(function(answerDescription){
+    peerConnection.setLocalDescription(answerDescription);
+    window.answerDescription =  answerDescription // use this on the other one
+  }, null, mediaConstraints);
+}
